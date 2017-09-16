3d596833cd578f4b70c2f3a4001415fab1072be8
Vnakas <vinicius_dilda@hotmail.com>
2017-09-16 14:40:39 -0300

    Add files via upload

--- heap.c
+++ heap.c
@@ -2,108 +2,115 @@
 #include<stdlib.h>
 #include"heap.h"
 
-    void inicializarHeap(Heap *h, int tam_max)
-    {
-      h->tam_atual = 0;
-      h->tam_max = tam_max;
-    }
-    int pai(int i)
-    {
-      return i/2;
-    }
+#define TRUE 1
+#define FALSE 0
 
-    int filhoEsquerda(int i)
-    {
-      return 2*i;
-    }
+int filho_Esquerda(int i)
+{
+    return 2i+1;
+}
 
-    int filhoDireita(int i)
-    {
-      return 2*i + 1;
-    }
+int filho_Direita(int i)
+{
+    return 2i+2;
+}
 
-    void destruirHeap(Heap * h)
+struct Heap_node* new_Min_Heap_Node(int v, int peso)
+{
+    struct Heap_node* min_Heap_Node = (struct Heap_node*) malloc(sizeof(struct Heap_node));
+    min_Heap_Node->vertice = v;
+    min_Heap_Node->peso = peso;
+    return min_Heap_Node;
+}
+struct Heap* create_Min_Heap(int tam_max)
+{
+    struct Heap* minHeap =(struct Heap*) malloc(sizeof(struct Heap));
+    minHeap->pos = (int *)malloc(tam_max * sizeof(int));
+    minHeap->tam_atual = 0;
+    minHeap->tam_max = tam_max;
+    minHeap->matriz =(struct Heap_node**) malloc(tam_max * sizeof(struct Heap_node*));
+    return minHeap;
+}
+int is_In_MinHeap(struct Heap *minHeap, int v)
+{
+   if (minHeap->pos[v] < minHeap->tam_atual)
+   {
+       return TRUE;
+   }
+   else
+   {
+        return FALSE;
+   }
+}
+int isEmpty(struct Heap* minHeap)
+{
+    if(minHeap->tam_atual == 0)
     {
-      free(h->prioridade);
-      h->tam_max=0;
-      h->tam_atual=0;
+        return TRUE;
     }
+    else
+    {
+        return FALSE;
+    }
+}
+void swap_MinHeap_Node(struct Heap_node** matriz_a, struct Heap_node** matriz_b)
+{
+    struct Heap_node* matriz_temp = *matriz_a;
+    *matriz_a = *matriz_b;
+    *matriz_b = matriz_temp;
+}
+void min_Heapify(struct Heap* minHeap, int i)
+{
+    int menor, esq, dir;
+    menor = i;
+    esq = filho_Esquerda(i);
+    dir = filho_Direita(i);
 
-    void minHeapify(Heap *h, int i)
+    if (esq < minHeap->tam_atual && minHeap->matriz[esq]->peso < minHeap->matriz[menor]->peso)
     {
-      int esq = filhoEsquerda(i);
-      int dir = filhoDireita(i);
-      int temp;
-      int menor = i;
-        if ((esq < h->tam_atual) && (h->prioridade[esq]<h->prioridade[i]))
-        {
-            menor = esq;
-        }
-        if ((dir < h->tam_atual) && (h->prioridade[dir]<h->prioridade[menor]))
-        {
-            menor = dir;
-        }
+        menor = esq;
+    }
+    if (dir < minHeap->tam_atual &&minHeap->matriz[dir]->peso < minHeap->matriz[menor]->peso)
+    {
+        menor = dir;
+    }
+    if (menor!=i)
+    {
+        //Nós a serem trocados na minheap
+        Heap_node *menor_Node = minHeap->matriz[menor];
+        Heap_node *i_Node = minHeap->matriz[i];
 
-        if (menor != i)
-        {
-            temp = h->prioridade[i];
-            h->prioridade[i] = h->prioridade[menor];
-            h->prioridade[menor] = temp;
-            minHeapify(h,menor);
-        }
-    }
+        //trocar Posições
+        minHeap->pos[menor_Node->vertice] = i;
+        minHeap->pos[i_Node->vertice] = menor;
 
-    void construirHeapMin(Heap *h)
-    {
-      int i;
-      int metadeTamanho = h->tam_atual/2;
-      for (i=metadeTamanho;i>0;i--)
-      {
-            minHeapify(h,i);
-      }
-    }
+        // Trocar Nós
+        swap_MinHeap_Node(&minHeap->matriz[menor], &minHeap->matriz[i]);
 
-    int inserirForaDeOrdem(Heap * h, int valor)
-    {
-        if (h->tam_atual< h->tam_max)
-        {
-            h->prioridade[h->tam_atual] = valor;
-            h->tam_atual = h->tam_atual+1;
-            return 1;
-        }
-        return 0;
+        min_Heapify(minHeap, menor);
     }
+}
+struct Heap_node* retirar_Min(struct Heap* minHeap)
+{
+    if (isEmpty(minHeap))
+    {
+        return NULL;
+    }
+    // Gurdar a raiz do nó
+    struct Heap_node* raiz = minHeap->matriz[0];
 
-    void imprimirArranjo(Heap h)
-    {
-      int tamanho = h.tam_atual;
-      int i;
-      for (i=0;i<tamanho;i++)
-      {
-          printf("%.2f\t",h.prioridade[i]);
+    // Troca o nó raiz com o ultímo nó
+    struct Heap_node* ultimo_Node = minHeap->matriz[minHeap->tam_atual - 1];
+    minHeap->matriz[0] = ultimo_Node;
 
-      }
-    }
-    int inserirHeap(Heap *h, int chave)
-    {
-        int i, temp;
-        if (h->tam_atual == h->tam_max)
-        {
-            return 0;
-        }
-        i = h->tam_atual;
-        h->prioridade[i] = chave;
-        h->tam_atual = h->tam_atual+1;
-        while ((i>1) && (h->prioridade[pai(i)]<h->prioridade[i]))
-        {
-            temp = h->prioridade[i];
-            h->prioridade[i] = h->prioridade[pai(i)];
-            h->prioridade[pai(i)] = temp;
-            i = pai(i);
-        }
-            return 1;
-    }
-    int Min_valor(Heap *h)
-    {
-        return h->prioridade[1];
-    }
+    // Atualizar a posicao do ultimo nó
+    minHeap->pos[raiz->vertice] = minHeap->tam_atual-1;
+    minHeap->pos[ultimo_Node->vertice] = 0;
+
+    // Reduz tamanho atual  heap e da raiz heapify
+    minHeap->tam_atual = minHeap->tam_atual-1;
+    min_Heapify(minHeap,0);
+
+    return raiz;
+}
+
