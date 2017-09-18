6c3b054575890d2b664aa13a6a8fd8fbf7832f23
Vnakas <vinicius_dilda@hotmail.com>
2017-09-17 23:28:08 -0300

    Add files via upload

--- heap.h
+++ heap.h
@@ -1,25 +1,19 @@
 #include<stdio.h>
 
-    typedef struct Heap_node
-    {
-        int vertice;
-        int peso;
-    }Heap_node;
+typedef struct node {
+    int idvertice;
+    int pesoaresta;
+} node ;
 
-    typedef struct Heap
-    {
-        int tam_max;
-        int tam_atual;
-        int *pos;
-        struct Heap_node **matriz;
-    }Heap;
+typedef struct minHeap {
+    int size ;
+    node *elem ;
+} minHeap ;
 
-int filho_Esquerda(int i);
-int filho_Direita(int i);
-struct Heap_node* new_Min_Heap_Node(int v, int peso);
-struct Heap* create_Min_Heap(int tam_max);
-int is_In_MinHeap(struct Heap *minHeap, int v);
-int isEmpty(struct Heap* minHeap);
-void swap_MinHeap_Node(struct Heap_node** matriz_a, struct Heap_node** matriz_b);
-void min_Heapify(struct Heap* minHeap, int i);
-struct Heap_node* retirar_Min(struct Heap* minHeap);
+minHeap initMinHeap(int size);
+void swap(node *n1, node *n2);
+void heapify(minHeap *hp, int i);
+void buildMinHeap(minHeap *hp, int *arr, int size);
+void insertNode(minHeap *hp, int v, int peso);
+void deleteNode(minHeap *hp);
+void deleteMinHeap(minHeap *hp);
