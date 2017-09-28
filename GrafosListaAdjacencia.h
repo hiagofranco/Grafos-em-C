d7bc70b98dde9d02733fbb8a0b123663969b2d71
cptziltoid <31487435+cptziltoid@users.noreply.github.com>
2017-09-28 17:38:05 -0300

    Add files via upload

--- GrafosListaAdjacencia.h
+++ GrafosListaAdjacencia.h
@@ -40,12 +40,6 @@ typedef struct tgrafo
 /* Funcoes utilizadas */
 void inicializaGrafo(tgrafo *grafo, int num_vertices);
 void insere_aresta(tgrafo *grafo, svertice v,svertice u, tpeso peso);
-void retira_aresta(tgrafo *grafo, svertice v, svertice u);
-int existe_aresta(tgrafo *grafo, svertice v,svertice u);
-void libera_grafo(tgrafo *grafo);
-int existe_adj(tgrafo *grafo, svertice v);
-tapontador primeiro_adj(tgrafo *grafo, svertice v);
-tapontador prox_adj(tgrafo *grafo, svertice v);
 void insere_egressos(tgrafo *grafo,svertice v,int egressos);
 void printaGrafo(tgrafo *grafo,int num_vertices);
 int quantidadeDeVertices(tgrafo *grafo);
