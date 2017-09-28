442200305df1c3d1f4756d09d90306d32c5adb48
Vnakas <vinicius_dilda@hotmail.com>
2017-09-28 18:56:48 -0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,10 +1,14 @@
 #include<stdio.h>
 #include"GrafosListaAdjacencia.h"
 #include"GrafosBusca.h"
+#define TRUE 1
+#define FALSE 0
+
 
 int main()
 {
     int i;
+    int aux;
     int id1,id2;
     int n_cidades;
     int qt_estrada;
@@ -12,20 +16,106 @@ int main()
     int egressos_cidade;
     tgrafo Grafo;
 
-    scanf("%d %d",&n_cidades,&qt_estrada);
-    //Inicialzia a matriz
+    do
+    {
+        aux = TRUE;
+        printf("Digite o numero de cidades e o numero de estradas em ordem\n");
+        //Inicialzia a matriz
+        scanf("%d %d",&n_cidades,&qt_estrada);
+        if(n_cidades<2 || n_cidades>50)
+        {
+            printf("Erro: Numero de cidades e incompativel\n");
+            aux = FALSE;
+        }
+        if(qt_estrada<1 || qt_estrada>(n_cidades*n_cidades))
+        {
+            printf("Erro: Numero de arestas e incompativel\n");
+            aux = FALSE;
+        }
+        if(aux==FALSE)
+        {
+            system("pause");
+            system("cls");
+        }
+    }while(aux==FALSE);
     inicializaGrafo(&Grafo,n_cidades);
 
     for(i=0;i<n_cidades;i++)
     {
     //escaneia os egressos para cada cidade (n-1) vezes
-        scanf("%d",&egressos_cidade);
+        do
+        {
+            aux = TRUE;
+            printf("Digite o numero de egressos da cidade de %d\n",i);
+            scanf("%d",&egressos_cidade);
+            if(egressos_cidade<0 || egressos_cidade>50)
+            {
+                printf("Erro: Numero de egressos incompativel\n");
+                aux = FALSE;
+            }
+            if(aux==FALSE)
+            {
+                system("pause");
+                system("cls");
+            }
+        }while(aux==FALSE);
         insere_egressos(&Grafo,i,egressos_cidade);
     }
+    printf("\nProcesso de insercao de Distancias entre arestas\n\n");
     for(i=0;i<qt_estrada;i++)
     {
         //estrada de tamanho d que liga Id1 a Id2
-        scanf("%d %d %f", &id1,&id2,&estrada_tam);
+        do
+        {
+            aux = TRUE;
+            printf("Digite a cidade de origem:\n");
+            scanf("%d",&id1);
+            if(id1<0 || id1>=n_cidades)
+            {
+                printf("Erro: O ID da cidade de origem e incompativel\n");
+                aux = FALSE;
+            }
+            if(aux==FALSE)
+            {
+                system("pause");
+                system("cls");
+            }
+        }while(aux==FALSE);
+
+        do
+        {
+            aux = TRUE;
+            printf("Digite a cidade de destino:\n");
+            scanf("%d",&id2);
+            if(id2<0 || id2>=n_cidades)
+            {
+                printf("Erro: O ID da cidade de destino e incompativel\n");
+                aux = FALSE;
+            }
+            if(aux==FALSE)
+            {
+                system("pause");
+                system("cls");
+            }
+        }while(aux==FALSE);
+
+        do
+        {
+            aux = TRUE;
+            printf("Digite a distancia entre as cidades %d e %d:\n",id1,id2);
+            scanf("%f", &estrada_tam);
+            if(estrada_tam<0 || estrada_tam>100)
+            {
+                printf("Erro: A distancia entre as estradas e incompativel\n");
+                aux = FALSE;
+            }
+            if(aux==FALSE)
+            {
+                system("pause");
+                system("cls");
+            }
+        }while(aux==FALSE);
+
         insere_aresta(&Grafo,id1,id2,estrada_tam);
     }
     vertice_central(&Grafo);
